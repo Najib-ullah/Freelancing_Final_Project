a4f6a05b6e97d187afc6b3b8b05d16060ed1ac10
zmaryalaiali <zmaryalaiali@gmail.com>
2024-03-04 14:10:13 +0430

    first upload of the project

---
+++ app/src/main/java/net/kdru/freelancing/Log_in_Activity.java
@@ -0,0 +1,14 @@
+package net.kdru.freelancing;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Log_in_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_in);
+    }
+}
